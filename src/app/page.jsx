b4cc2a4f747bf3b2026aa549e2a14bf5f4e6ff04
petyrK <pk.peterirungu@gmail.com>
2024-12-07 22:30:54 -0600

    new hero message

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -20,7 +20,7 @@ const Homepage = () => {
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
           {/* TITLE */}
           <h1 className="text-4xl md:text-6xl font-bold">
-            Devops Excellence, Building Tomorrow&#39;s Cloud Networks.
+            Networking Excellence, Building Tomorrow&#39;s IP Networks.
           </h1>
           {/* DESC */}
           <p className="md:text-xl">
